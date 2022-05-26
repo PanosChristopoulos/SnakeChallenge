24af5db0d433b094e22fceb7c69ae4f2726aba03
PanosChristopoulos <56166480+PanosChristopoulos@users.noreply.github.com>
2022-05-26 03:55:59 +0200

    minor changes in game.py

--- game.py
+++ game.py
@@ -1,4 +1,18 @@
 from main import snakeGame      
+import time
+
+
+
+def calculateSnakeMoves(board,snake,depth):
+
+    snakeGame_ = snakeGame(board,snake,depth)
+    funcReturn = snakeGame_.numberOfAvailableDifferentPaths()
+
+    return funcReturn[1]
+
+
+start_time = time.time()
+
 
 sampleSnakeA = [[2,2], [3,2], [3,1], [3,0], [2,0], [1,0], [0,0]]
 sampleBoardA = [4, 3]
@@ -12,11 +26,29 @@ sampleSnakeC = [[5,5], [5,4], [4,4], [4,5]]
 sampleBoardC = [10, 10]
 sampleDepthC = 4
 
+calculateSnakeMoves(sampleBoardA,sampleSnakeA,sampleDepthA)
+calculateSnakeMoves(sampleBoardB,sampleSnakeB,sampleDepthB)
+calculateSnakeMoves(sampleBoardC,sampleSnakeC,sampleDepthC)
 
+
+#Detailed usage of caluclateSnakeMoves
+"""
 sampleSnakeGame = snakeGame(sampleBoardA,sampleSnakeA,sampleDepthA)
 sampleSnakeGame.numberOfAvailableDifferentPaths()
+
 sampleSnakeGame = snakeGame(sampleBoardB,sampleSnakeB,sampleDepthB)
 sampleSnakeGame.numberOfAvailableDifferentPaths()
 
 sampleSnakeGame = snakeGame(sampleBoardC,sampleSnakeC,sampleDepthC)
 sampleSnakeGame.numberOfAvailableDifferentPaths()
+
+"""
+
+
+stop_time = time.time()
+totalTime = stop_time - start_time
+
+print("Calculated sample snakes' paths in",round(totalTime, 5),'seconds')
+
+
+#calculateSnakeMoves(,,,)
